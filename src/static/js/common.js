94dc4104eef247a89a7db1b04fd62e9b6e3c115c
devsequence <devsequencce@gmail.com>
2024-08-09 14:34:08 +0300

    first commit

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -47,3 +47,46 @@ window.addEventListener('progressEvent', (e) => {
     $('.community-top').css('opacity', '1').removeClass('active');
   }
 });
+$('.play-btn').on('click', function (e) {
+  e.preventDefault();
+  const t = $(this);
+  const tVideo = t.prev().attr('src');
+  $('body').addClass('scroll');
+  $('.popup-video').addClass('active').find('video').attr('src', tVideo);
+  $('.popup-video').find('video').attr('autoplay', 'true');
+});
+$('.close-popup').on('click', function (e) {
+  var $this = $(this);
+  $this.parent().removeClass('active');
+  $('body').removeClass('scroll');
+  $('.popup-video').removeClass('active').find('video').attr('src', '');
+  $('.popup-video').find('video').attr('autoplay', 'false');
+});
+
+window.addEventListener("progressEvent", (e) => {
+  const { progress } = e.detail;
+  if(progress >= 0.2539680028129395){
+    $('.chart-container').addClass('active');
+    $('.circle-chart-circle').css('animation', 'circle-chart-fill 1.35s reverse');
+    $('.circle-chart-one').css('animation', 'circle-chart-fill 2s reverse');
+    $('.circle-chart-two').css('animation', 'circle-chart-fill 1.7s reverse');
+    $('.circle-chart-three').css('animation', 'circle-chart-fill 1.7s  reverse');
+    setTimeout(function (e) {
+      $('.chart-container-info').addClass('active');
+    }, 2000)
+  } else{
+    $('.chart-container, .chart-container-info').removeClass('active');
+    $('.circle-chart-circle').css('animation', ' none');
+    $('.circle-chart-one').css('animation', 'none');
+    $('.circle-chart-two').css('animation', 'none');
+    $('.circle-chart-three').css('animation', 'none');
+  }
+});
+window.addEventListener("progressEventComparison", (e) => {
+  const { progress } = e.detail;
+  if(progress >= 0.2539680028129395){
+    $('.comparison-wrap').addClass('active');
+  } else{
+    $('.comparison-wrap').removeClass('active');
+  }
+});
